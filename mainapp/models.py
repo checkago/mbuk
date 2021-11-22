5cbcf80ee1827d53ff63612881a00b3ead0a557e
Eugene Checkoy <mfalcon@mail.ru>
2021-11-22 17:17:15 +0300

    Kadr Docs Guide

--- mainapp/models.py
+++ mainapp/models.py
@@ -85,6 +85,9 @@ class Position(models.Model):
 class Employee(models.Model):
     # Связь с пользователем
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True, verbose_name='Должность')
+    branch_office = models.ForeignKey(BranchOffice, on_delete=models.SET_NULL, null=True, verbose_name='Филиал')
+    salary = models.DecimalField(max_digits=6, decimal_places=2, blank=True, verbose_name='Оклад')
 
     # Персональные данные
     MALE = 'Мужской'
@@ -130,7 +133,9 @@ class Employee(models.Model):
     place_of_stay_address = models.ForeignKey('Address', blank=True, on_delete=models.CASCADE,
                                               verbose_name='Адрес временной регистрации')
 
-    # СТАЖ и ПФР
+    # СВЕДЕНИЯ о РАБОТЕ и СТАЖ
+    adopted_date = models.DateField(auto_now_add=True, blank=True, verbose_name='Принят')
+    dismissed_date = models.DateField(blank=True, verbose_name='Дата увольнения')
     experience_before = models.IntegerField(max_length=2, verbose_name='Предыдущий стаж')
     digital_work_book = models.BooleanField(default=False, verbose_name='"Электронная')
     paper_work_book = models.BooleanField(default=False, verbose_name='Бумажная')
